0777c339ce175c74c99338c0af4459527c4e056d
Avi Peretz <aviprz91@gmail.com>
2024-03-12 17:49:46 +0200

    Extend the schema definition

--- src/schema.ts
+++ src/schema.ts
@@ -2,9 +2,32 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 // @ts-ignore
 import typeDefs from './schema.graphql';
 
+type Link = {
+  id: string;
+  url: string;
+  description: string;
+};
+
+function getLinks(): Link[] {
+  return [
+    {
+      id: '1',
+      url: 'www.sample.com',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+  ];
+}
+
 const resolvers = {
   Query: {
-    info: () => 'Test',
+    info: () => `This is A GraphQL-Fastify server`,
+    feed: () => getLinks(),
+  },
+
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url,
   },
 };
 
